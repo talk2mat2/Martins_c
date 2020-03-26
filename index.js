0e16376f3ad68feedea3f0e970cb7dc2caefcb85
talk2mat2 <62118678+talk2mat2@users.noreply.github.com>
2020-03-26 23:18:35 +0100

    Add files via upload
    my JavaScript file

---
+++ index.js
@@ -0,0 +1,13 @@
+var myName = "martins chukwuma";
+var myCourses = ["python","html","css","javascript","node.js","ui/ux"];
+
+console.log(myName);
+console.log(myCourses);
+
+/*the number of my courses is 6 which is an even number
+so display all even numbers between 1 -200 */
+
+for(let i = 1; i<=200; i++) {
+    if (i%2==0) {console.log(i);
+    } 
+}
