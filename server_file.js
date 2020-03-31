f8047e0af94fc26400efe949e6608c8e4300c6e6
talk2mat2 <62118678+talk2mat2@users.noreply.github.com>
2020-03-31 02:19:29 +0200

    Update server_file.js

--- server_file.js
+++ server_file.js
@@ -1,5 +1,5 @@
 let http = require('http');
-var myHtml = '<html><head><title>hy form</title></head><body><form action="/message" method="post"><label for="message"> Please enter a message below:</label> <br/><br/><textarea rows="4" cols="50" name="message" id="message"placeholder="Describe yourself here..."></textarea><br/><br/><input type="submit" value="Submit button"/></form></body></html ';
+var myHtml = '<html><head><title>hy form</title></head><body><form action="/message" method="post"><label for="message"> Please enter a message below:</label> <br/><br/><textarea rows="4" cols="50" name="message" id="message"placeholder="where user submits their message"></textarea><br/><br/><input type="submit" value="Submit button"/></form></body></html ';
 
 let handleRequest = (req, res) => {
     res.writeHead(200, {
