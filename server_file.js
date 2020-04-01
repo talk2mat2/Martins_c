39526a237f4758f933c74dd2e1d19140abee594c
talk2mat2 <62118678+talk2mat2@users.noreply.github.com>
2020-04-01 13:40:42 +0200

    Update server_file.js

--- server_file.js
+++ server_file.js
@@ -3,13 +3,24 @@ var fs = require('fs')
 var myHtml = '<html><head><title>hy form</title></head><body><form action="/message" method="post"><label for="message"> Please enter a message below:</label> <br/><br/><textarea rows="4" cols="50" name="message" id="message"placeholder="Describe yourself here..."></textarea><br/><br/><input type="submit" value="Submit button"/></form></body></html ';
 
 let handleRequest = (req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    res.write(myHtml);
-    res.end();
-    let data = req.body;
-    fs.appendFileSync('./message.txt', data);
+    if (req.method === 'POST') {
+       
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString(); 
+        });
+        req.on('end', () => {
+            fs.appendFileSync('message.txt',body);
+            res.end('ok');
+        });
+    }
+    else {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        res.write(myHtml);
+        res.end();
+    }
 };
- 
+
 http.createServer(handleRequest).listen(8080);
